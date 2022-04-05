092a6d3df3c89c61f49be5c82730f9bf6c9bea0b
Joaquin Cavanna <jcavanna14@gmail.com>
2022-04-04 21:26:07 -0300

    Operadores Avanzados

---
+++ OperadorAvanzado.js
@@ -0,0 +1,12 @@
+
+const persona = {
+
+nombre: "Pepe",
+edad: prompt("Ingrese su edad: ")
+
+}
+
+const verificarEdad = (persona.edad >= 18);
+
+verificarEdad ? alert("Puedes pasar, eres mayor") : alert("Volvé al jardin pibe.");
+
